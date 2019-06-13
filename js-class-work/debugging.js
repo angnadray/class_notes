13e0cc8174dfadf5ba448c5de9478cbed0189d60
Ang <naiadelle@freenet.de>
2019-06-13 11:57:35 +0200

    debugging first basics

---
+++ js-class-work/debugging.js
@@ -0,0 +1,31 @@
+// TYPE OF ERRORS
+
+// syntax error
+
+/* a reserved key word is written wrong */
+
+// runtime error
+
+/* there is no syntax error but it creates an infinite loop */
+
+// semantic errors
+
+/* when there is no error on the terminal but we are not getting the desire result, 
+for example using the wrong mathematic operator etc */
+
+// reference error
+
+/* whe variables are not definedv for example */
+
+// TRY and CATCH
+
+/* code timed to be executed at a later point will not work with try and catch because
+try and catch is synchronized meaning it will be executed at the same time */
+
+// JSON
+
+/* ????? */
+
+// new Error
+
+/* to create our own errors  */
