794866154379a9d2da0fcac15ce27400c8605548
Ang <naiadelle@freenet.de>
2019-07-01 15:31:42 +0200

    event delegation js file with examples

---
+++ browser/DOM/eventDeligation_basics.js
@@ -0,0 +1,25 @@
+document.getElementById("parent-list").addEventListener('click', function (event){
+    if (event.target && event.target.nodeName == "LI") {
+        console.log('event.target:', event.target);
+        console.log('event.target.nodeName', event.target.nodeName);
+        console.log('event', event);
+         
+        console.log(`list item ${event.target.id.replace('post-', '')} was clicked`);
+        console.log('event.target.id',event.target.id);
+        
+    }
+    
+});
+
+document.getElementById('myDiv').addEventListener('click', function(event){
+    console.log('event.target', event.target);
+    if(event.target && event.target.matches('a.classA')){
+        console.log('Anchor element clicked!');
+        
+    }
+
+    console.log('event', event);
+    
+    
+
+})
