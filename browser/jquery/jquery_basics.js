7a38842e66de380b7ad1b48747ba725abb5f6372
Ang <naiadelle@freenet.de>
2019-07-04 14:28:19 +0200

    jquery basics from teacher, js file

---
+++ browser/jquery/jquery_basics.js
@@ -0,0 +1,26 @@
+/* if we see this $() it means it is jQuery */
+
+// $('#demo').html('Hello freaks!');
+
+// jQuery SELECTORS
+// $(document).ready(function() {
+//     $('#demo').html('Hello freaks!');
+// })
+
+// EVENTS
+$(document).ready(function() {
+    $('#trigger').click(function() {
+        $('#demo').html('hi freaks');
+    })
+})
+
+// with 'on' we can select multiple events
+$(document).ready(function() {
+    $('#greeting').on('mouseenter mouseleave', function(event) {
+        if(event.type === 'mouseenter') {
+        $('#greeting').css('color', 'red');
+        } else {
+            $('#greeting').css('color', 'green');
+        }
+    })
+})
